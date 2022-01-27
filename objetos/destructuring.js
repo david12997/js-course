86c7825d51bc938962fc0f668acc729db7f7694a
david12997 <david12997@outlook.com>
2022-01-27 14:06:04 -0500

    destructuring como parametro

--- objetos/destructuring.js
+++ objetos/destructuring.js
@@ -3,13 +3,21 @@
 const persona ={
 
     nombre:"Jose",
-    apellido:"Castañeda"
+    apellido:"Castañeda",
+    edad:20
 }
 
 let { nombre, apellido} = persona;
 
 console.log(nombre, apellido);
 
+//destructurin como argumento para una funcion
+function ImprimirPersona({nombre,apellido,edad}){
+
+    console.log(nombre, apellido, edad);
+}
+
+ImprimirPersona(persona);
 
 //destructuring array
 const Array1 = [0,1,2,3,true,'Juan'];
