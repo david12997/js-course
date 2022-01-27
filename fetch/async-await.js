6fc3be644a357bacd9ea3334b6007a313441e2b1
david12997 <david12997@outlook.com>
2022-01-27 14:53:07 -0500

    manejo de catch async await

--- fetch/async-await.js
+++ fetch/async-await.js
@@ -2,26 +2,27 @@ import fetch from 'node-fetch';
 
 async function GetData(){
 
-    console.log('Esperando datos..');
+    console.log('Inicio Esperando datos..');
 
     try{
 
         const data = await fetch('https://runalotus.com/colombia/wp-json/wp/v2/media');
         const response = await data.json();
     
-        console.log(response);
+       return response;
 
     }
     catch(error){
 
-        console.log(error);
+        throw error;
     }
-
 
 }
 
 console.log('antes de ejecutar getData')
 
 GetData()
+    .then(res=>console.log(res))
+    .catch(err=>console.log(err));
 
 console.log('DEspues de ejecutar getData')
